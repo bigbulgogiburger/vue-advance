90dc36f889516fa007de69ce89f373fb6810e4a1
bigbulgogiburger <dohoon321@gmail.com>
2023-09-05 14:21:31 +0900

    Vue 세팅중.

---
+++ vue-news/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!-- url이 만약  news-->
+    <tool-bar></tool-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ToolBar from "./components/ToolBar.vue";
+/* eslint-disable */
+export default {
+  components: { ToolBar },
+  methods: {
+    fetchData() {
+      console.log("hello");
+    },
+  },
+};
+</script>
+
+<style></style>
