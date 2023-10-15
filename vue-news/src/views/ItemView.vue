b506dc1b19add97b38b6f10e57538dd5d091b08e
bigbulgogiburger <dohoon321@gmail.com>
2023-10-15 22:44:38 +0900

    코드 수정

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -3,16 +3,11 @@
         <section>
             <user-profile :userInfo="fetchedItem">
                 <div slot="username">{{ fetchedItem.user }}</div>
-                <template slot="time">{{ fetchedItem.time_ago }}</template>
+                <router-link slot="username" :to="`/user/${fetchedItem.user}`">
+                    {{ fetchedItem.user }}
+                </router-link>
+                <template slot="time">{{ 'Posted '+ fetchedItem.time_ago }}</template>
             </user-profile>
-            <!--  사용자 상세 정보 -->
-            <!-- <div class="user-container">
-                <div><i class="fas fa-user"></i></div>
-                <div class="user-description">
-                    <router-link v-bind:to="`/user/${fetchedItem.user}`">{{ fetchedItem.user }}</router-link>
-                    <div class="time">{{ fetchedItem.time_ago }}</div>
-                </div>
-            </div> -->
         </section>
         <section>
             <h2>{{ fetchedItem.title }}</h2>
