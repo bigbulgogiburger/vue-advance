0ab13e62dca4633c466c7ed26eb1f11a1cf26ce6
bigbulgogiburger <dohoon321@gmail.com>
2023-10-15 22:43:51 +0900

    코드 수정

--- vue-news/src/components/UserProfile.vue
+++ vue-news/src/components/UserProfile.vue
@@ -14,7 +14,9 @@
                 <slot name="time">
                 <!-- 상위 컴포넌트에서 정의할 영역 -->
                 </slot>
-                <slot name="karma"></slot>
+                <slot name="karma">
+                <!-- 상위 컴포넌트에서 정의할 영역 --> 
+                </slot>
             </div>
         </div>
     </div>
